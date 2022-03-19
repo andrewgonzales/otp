ae9994c98a90731bb5db23d835f8ae8a0f6bbbb3
Andrew Gonzales <andrewmartingonzales@gmail.com>
2022-03-19 16:44:55 -0700

    Add dynamic truncation and modulo

--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,47 @@ type HmacSha1 = Hmac<Sha1>;
 
 fn main() {
     let secret = "abc";
-    let counter = 0;
+    let counter = 2;
 
     let hotp = truncate(make_hmac(secret.as_bytes(), counter));
-	println!("hotp: {}", hotp);
+    println!("hotp: {}", hotp);
 }
 
 // HMAC_SHA-1 -> 20 byte string
-fn make_hmac(secret: &[u8], counter: i32) -> &str {
-	let mut mac = HmacSha1::new_from_slice(secret).expect("Problem with secret, failed to initialize HMAC");
-	mac.update(&counter.to_be_bytes());
-	let result = mac.finalize();
+fn make_hmac(secret: &[u8], counter: i32) -> Vec<u8> {
+    let mut mac =
+        HmacSha1::new_from_slice(secret).expect("Problem with secret, failed to initialize HMAC");
+    mac.update(&counter.to_be_bytes());
+    let result = mac.finalize();
 
-	println!("result: {:?}", result.into_bytes());
-	"12345678"
+    result.into_bytes().to_vec()
 }
 
 // reduce to 4 byte string
 // then s to num mod 10^Digit
-fn truncate(hmac: &str) -> &str {
-	"123456"
+fn truncate(hmac: Vec<u8>) -> u32 {
+    println!("hmac: {:?}", hmac);
+    let dynamic_truncation = dynamic_truncation(hmac);
+
+    dynamic_truncation % u32::pow(10, 6)
+}
+
+// DT(String) // String = String[0]...String[19]
+// Let OffsetBits be the low-order 4 bits of String[19]
+// Offset = StToNum(OffsetBits) // 0 <= OffSet <= 15
+// Let P = String[OffSet]...String[OffSet+3]
+// Return the Last 31 bits of P
+
+// int offset   =  hmac_result[19] & 0xf ;
+// int bin_code = (hmac_result[offset]  & 0x7f) << 24
+//    | (hmac_result[offset+1] & 0xff) << 16
+//    | (hmac_result[offset+2] & 0xff) <<  8
+//    | (hmac_result[offset+3] & 0xff) ;
+fn dynamic_truncation(hmac: Vec<u8>) -> u32 {
+    let offset = (hmac[19] & 0xf) as usize;
+    let code = (hmac[offset] as u32 & 0x7f) << 24
+        | (hmac[offset + 1] as u32 & 0xff) << 16
+        | (hmac[offset + 2] as u32 & 0xff) << 8
+        | (hmac[offset + 3] as u32 & 0xff);
+    code
 }
