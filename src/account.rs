fa67242a88f0f3a325c6ae6584b5c3eb2b6c0278
Andrew Gonzales <andrewmartingonzales@gmail.com>
2022-03-25 20:20:09 -0700

    Set up command line and create account store with persistence

---
+++ src/account.rs
@@ -0,0 +1,60 @@
+extern crate toml;
+
+use std::collections::BTreeMap;
+use std::io::Result;
+use std::fs;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use serde::{Serialize, Deserialize};
+
+const FILE_PATH: &str = "src/data/accounts.txt";
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Account {
+    pub key: String,
+    pub counter: Option<i32>,
+}
+
+impl Account {
+    pub fn new(key: String) -> Self {
+        Account { key, counter: Some(0) }
+    }
+}
+
+pub struct AccountStore {
+    accounts: BTreeMap<String, Account>,
+}
+
+impl AccountStore {
+    pub fn new() -> Result<AccountStore> {
+		let file = File::open(FILE_PATH)?;
+		let mut buf_reader = BufReader::new(file);
+		let mut accounts_str = String::new();
+		buf_reader.read_to_string(&mut accounts_str)?;
+        let accounts: BTreeMap<String, Account> = toml::from_str(&accounts_str)?;
+        Ok(AccountStore { accounts })
+    }
+
+    pub fn get(&self, account_name: &str) -> Option<&Account> {
+        self.accounts.get(account_name)
+    }
+
+    pub fn list(&self) -> &BTreeMap<String, Account> {
+        &self.accounts
+    }
+
+    pub fn add(&mut self, account_name: String, account: Account) {
+        self.accounts.insert(account_name, account);
+    }
+
+    pub fn delete(&mut self, account_name: &str) -> Option<Account> {
+        self.accounts.remove(account_name)
+    }
+
+    pub fn save(&self) -> Result<()> {
+        let accounts_str = toml::to_string(&self.accounts).expect("Serialization failure");
+        fs::write(FILE_PATH, accounts_str)?;
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-extern crate rand;
+extern crate clap;
 extern crate data_encoding;
+extern crate rand;
 
+use clap::{arg, command, ArgMatches};
+use data_encoding::BASE32_NOPAD;
 use hmac::{Hmac, Mac};
+use rand::rngs::OsRng;
 use rand::RngCore;
 use sha1::Sha1;
-use std::fmt::Debug;
 use std::io::{Error, ErrorKind};
-use rand::rngs::OsRng;
-use data_encoding::BASE32;
+
+use crate::account::{Account, AccountStore};
+
+mod account;
 
 // HOTP https://datatracker.ietf.org/doc/html/rfc4226
 
@@ -19,32 +24,80 @@ use data_encoding::BASE32;
 
 type HmacSha1 = Hmac<Sha1>;
 
-#[derive(Debug)]
-struct HOTP<'a> {
-    secret: &'a str,
-    counter: i32,
+fn main() {
+	let account_store = AccountStore::new().expect("Unable to initialize store");
+    let cmd = command!("otp")
+        .about("HOTP client and server methods")
+        .version("v0.1.0")
+        .bin_name("otp")
+        .subcommand_required(true)
+        .subcommand(command!("server").arg(
+            arg!(-g --generate ... "Generates a 20 byte random base32 secret key").required(false),
+        ))
+        .subcommand(command!("client").args(&[
+            arg!(-a --account <NAME> "Account name to create").required(true),
+            arg!(-k --key <KEY> "Secret key").required(true).validator(is_base32_key),
+        ]));
+    let matches = cmd.get_matches();
+    match matches.subcommand() {
+        Some(("server", server_args)) => run_server(server_args),
+        Some(("client", client_args)) => run_client(client_args, account_store),
+        _ => unreachable!("No commands were supplied!"),
+    };
+
+    // let secret = "abc";
+    // let counter = 0;
+
+    // let mut hotp = HOTP::new(secret);
+    // println!("hotp: {:?}", hotp);
+    // let otp = get_hotp(secret, counter);
+    // println!("otp: {}", otp);
+    // let is_valid = validate_hotp(&mut hotp, 974315);
+    // println!("is valid: {:?}", is_valid);
+    // let is_valid2 = validate_hotp(&mut hotp, 974315);
+    // println!("is valid 2: {:?}", is_valid2);
+    // let new_secret = generate_secret();
+    // println!("new_secret = {:?}", new_secret);
 }
 
-impl HOTP<'_> {
-    fn new(secret: &'static str) -> Self {
-        HOTP { secret, counter: 0 }
+fn run_server(server_args: &ArgMatches) {
+    if server_args.is_present("generate") {
+        let new_secret_key = generate_secret();
+        println!("new secret key = {:?}", new_secret_key);
     }
 }
 
-fn main() {
-    let secret = "abc";
-    let counter = 0;
+fn run_client(client_args: &ArgMatches, mut account_store: AccountStore) {
+	println!("account_store = {:?}", account_store.list());
 
-    let mut hotp = HOTP::new(secret);
-    println!("hotp: {:?}", hotp);
-    let otp = get_hotp(secret, counter);
-    println!("otp: {}", otp);
-	let is_valid = validate_hotp(&mut hotp, 974315);
-	println!("is valid: {:?}", is_valid);
-	let is_valid2 = validate_hotp(&mut hotp, 974315);
-	println!("is valid 2: {:?}", is_valid2);
-	let new_secret = generate_secret();
-	println!("new_secret = {:?}", new_secret);
+    let account_name = client_args.value_of("account").unwrap();
+    let key = client_args.value_of("key").unwrap();
+
+    println!("account = {:?}", account_name);
+    println!("key = {:?}", key);
+
+
+	let account = Account::new(String::from(key));
+	println!("account = {:?}", account);
+
+    if account_store.get(account_name).is_some() {
+        println!("Account already exists");
+    } else {
+        account_store.add(account_name.to_string(), account);
+        match account_store.save() {
+            Ok(_) => println!("Account successfully created"),
+            Err(err) => eprintln!("{}", err),
+        }
+    }
+}
+
+// Validate key provided in arguments is a valid base32 encoding
+fn is_base32_key(value: &str) -> Result<(), String> {
+    let value = value.to_uppercase();
+    match BASE32_NOPAD.decode(value.as_bytes()) {
+        Ok(_) => Ok(()),
+        Err(_) => Err(String::from("the key is not a valid base32 encoding")),
+    }
 }
 
 fn get_hotp(secret: &str, counter: i32) -> u32 {
@@ -52,15 +105,19 @@ fn get_hotp(secret: &str, counter: i32) -> u32 {
     truncate(hmac)
 }
 
-fn validate_hotp(hotp: &mut HOTP, code: u32) -> Result<u32, Error> {
-    let expected_code = get_hotp(hotp.secret, hotp.counter);
-	println!("expected: {}", expected_code);
-	println!("entered: {}, {}", code, code == expected_code);
+fn validate_hotp(account: &mut Account, code: u32) -> Result<u32, Error> {
+	let counter = match account.counter {
+		Some(value) => value,
+		None => 0,
+	};
+    let expected_code = get_hotp(&account.key, counter);
+    println!("expected: {}", expected_code);
+    println!("entered: {}, {}", code, code == expected_code);
     let result = match code {
         n if n == expected_code => {
-			hotp.counter += 1;
-			Ok(code)
-		},
+            account.counter = Some(counter + 1);
+            Ok(code)
+        }
         _ => Err(Error::new(ErrorKind::PermissionDenied, "Code didn't match")),
     };
 
@@ -108,7 +165,7 @@ fn dynamic_truncation(hmac: Vec<u8>) -> u32 {
 
 // Generate a 20 byte random base32 string
 fn generate_secret() -> String {
-	let mut dest = [0u8; 20];
-	OsRng.fill_bytes(&mut dest);
-	BASE32.encode(&dest)
+    let mut dest = [0u8; 20];
+    OsRng.fill_bytes(&mut dest);
+    BASE32_NOPAD.encode(&dest)
 }
